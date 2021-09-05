9ff0fdce65240aed12da98ec1219076b5377f2dc
bdxchen <bdexchen@gmail.com>
2021-09-05 22:35:43 +0800

    状态机

---
+++ finite-state-machine/1/match.js
@@ -0,0 +1,11 @@
+// 查找字符串 'a'
+function match(string) {
+  for(let c of string) {
+    if (c == 'a') {
+      return true
+    }
+  }
+  return false
+}
+
+console.log(match('I am groot'))
---
+++ finite-state-machine/2/match.js
@@ -0,0 +1,17 @@
+// 查找字符串 'ab'
+
+function match(string) {
+  let foundA = false
+  for(let c of string) {
+    if (c == 'a') {
+      foundA = true
+    } else if (foundA && c == 'b') {
+      return true
+    } else {
+      foundA = false
+    }
+  }
+  return false
+}
+
+console.log(match('I abm groot'))
